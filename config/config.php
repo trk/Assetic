d142ed0ffbbffd86ae93d23e890b4037c82a84e3
Iskender TOTOĞLU <iskender@altivebir.com.tr>
2014-05-08 13:24:36 +0300

    Version Update

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@ $config['module']['assetic'] = array
 (
 	'module' => "Assetic",
     'name' => "Assetic",
-    'description' => "Asset Manager For Ionize CMS.<br/><b>Version : </b>1.0<br/><b>Author : </b>İskender TOTOĞLU<br/><b>Company : </b>ALTI VE BIR IT.<br/><b>Website : </b>http://www.altivebir.com.tr",
+    'description' => "Asset Manager For Ionize CMS.<br/><b>Version : </b>1.0.1<br/><b>Author : </b>İskender TOTOĞLU<br/><b>Company : </b>ALTI VE BIR IT.<br/><b>Website : </b>http://www.altivebir.com.tr",
     'author' => "İskender TOTOĞLU - ALTI ve BİR IT",
-	'version' => "1.0",
+	'version' => "1.0.1",
 
 	'uri' => 'assetic',
     'has_admin'=> FALSE,
